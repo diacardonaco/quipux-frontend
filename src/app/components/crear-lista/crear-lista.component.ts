bf46c7b686ce46725cafefe53b575a388b02008b
DESKTOP-GMRKOO2\Asus <diacardonaco@gmail.com>
2022-09-21 18:05:54 -0500

    HUF-02: adjust component create list.

--- src/app/components/crear-lista/crear-lista.component.ts
+++ src/app/components/crear-lista/crear-lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { List } from 'src/app/models/list.model';
+import { ListsService } from 'src/app/service/lists.service';
 
 @Component({
   selector: 'app-crear-lista',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearListaComponent implements OnInit {
 
-  constructor() { }
+  createForm!: FormGroup;
+  listsObtained: List[] | undefined;
+  
+  dataList!: any;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private listsService: ListsService) {}
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+
+  onSubmit() {
+    if (this.createForm && this.createForm.valid) {
+      console.log("Submitting form: ", this.createForm.value);
+      this.dataList =  this.createForm.value;
+      this.listsService.createList(new List(this.dataList.id, this.dataList.nombre, this.dataList.descripcion, 
+        this.dataList.canciones)).subscribe(list => this.listsObtained?.push(list));
+    } else {
+      console.error("Form is invalid!");
+    }
+  }
+
+  private buildForm() {
+    this.createForm = this.fb.group({
+      id: ["", Validators.required],
+      nombre: ["", Validators.required],
+      descripcion: ["", Validators.required],
+      canciones: ["", Validators.required]
+    });
   }
 
 }
